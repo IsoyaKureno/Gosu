e6042822eb337b0ec34398059463872ec78ad789
quent176 <quentin7611@yahoo.fr>
2017-11-13 12:17:00 +0100

    Add Main Activity Layout

--- app/src/main/java/fr/wildcodeschool/florianscoffee/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/florianscoffee/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.wildcodeschool.florianscoffee;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView _imageViewLogoMenu = (ImageView) findViewById(R.id.imageViewLogoMenu);
+        TextView _textViewDescriptionMenu = (TextView) findViewById(R.id.textViewDescriptionMenu);
+
+        _textViewDescriptionMenu.setPaintFlags(_textViewDescriptionMenu.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
     }
 }

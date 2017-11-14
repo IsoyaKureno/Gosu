739e1a9a0630e475216cb20ce9ff234f32b1cdc2
quent176 <quentin7611@yahoo.fr>
2017-11-14 16:58:40 +0100

    Maquette basique pour menu

--- app/src/main/java/fr/wildcodeschool/florianscoffee/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/florianscoffee/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,13 +20,14 @@ public class MainActivity extends AppCompatActivity {
         ImageView _imageViewLogoMenu = (ImageView) findViewById(R.id.imageViewLogoMenu);
         TextView _textViewDescriptionMenu = (TextView) findViewById(R.id.textViewDescriptionMenu);
         TextView _textViewShowMap = (TextView) findViewById(R.id.textViewShowMap);
+        Button _buttonTESTPrivatisation = (Button) findViewById(R.id.buttonTESTPrivatisation);
 
         _textViewShowMap.setPaintFlags(_textViewShowMap.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         _textViewShowMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                Intent intent = new Intent(MainActivity.this, PrivatisationUserActivity.class);
                 startActivity(intent);
             }
         });
